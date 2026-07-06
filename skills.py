5691b08a901a9ba4bcc8253d4f83874a1ec14943
anushaaenugu1107-art <anushaaenugu1107@gmail.com>
2026-07-06 21:42:51 +0530

    Create skills.py

---
+++ skills.py
@@ -0,0 +1,25 @@
+job_roles = {
+
+    "Software Engineer": [
+        "python",
+        "sql",
+        "git",
+        "data structures",
+        "oops"
+    ],
+
+    "Frontend Developer": [
+        "html",
+        "css",
+        "javascript",
+        "react"
+    ],
+
+    "Data Scientist": [
+        "python",
+        "machine learning",
+        "numpy",
+        "pandas",
+        "sql"
+    ]
+}
